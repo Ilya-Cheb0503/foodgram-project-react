53fc74c61152d017da9d32ee45c939f9ec419f86
Ilya Cheban <cheb.ilya05@yandex.ru>
2023-04-22 15:26:21 +0300

    expe_2.1

--- backend/api/views.py
+++ backend/api/views.py
@@ -225,18 +225,17 @@ class RecipesViewSet(viewsets.ModelViewSet):
 
         # return Recipe.objects.all()
 
-        is_favorited = self.request.query_params.get('is_favorited')
+        is_favorited = self.request.query_params.get('is_favorited') or 0
 
-        if is_favorited is True:
+        if int(is_favorited) == 1:
             return Recipe.objects.filter(
                 favorites__user=self.request.user
             )
 
         is_in_shopping_cart = self.request.query_params.get(
-            'is_in_shopping_cart'
-        )
+            'is_in_shopping_cart') or 0
 
-        if is_in_shopping_cart is False:
+        if int(is_in_shopping_cart) == 1:
             return Recipe.objects.filter(
                 cart__user=self.request.user
             )
